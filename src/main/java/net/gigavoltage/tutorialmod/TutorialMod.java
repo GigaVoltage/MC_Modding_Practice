640e1e2f8e3053aab8c92b4beed621b16f8f1f25
GigaVoltage <128551694+GigaVoltage@users.noreply.github.com>
2023-03-21 19:26:12 -0500

    Created Zircon, ore, and creative tab

--- src/main/java/net/gigavoltage/tutorialmod/TutorialMod.java
+++ src/main/java/net/gigavoltage/tutorialmod/TutorialMod.java
@@ -1,8 +1,12 @@
 package net.gigavoltage.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.gigavoltage.tutorialmod.item.ModCreativeModeTab;
+import net.gigavoltage.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,19 +22,28 @@ public class TutorialMod {
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    //Test
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        if (event.getTab() == ModCreativeModeTab.TUTORIAL_TAB)
+            event.accept(ModItems.ZIRCON);
+            event.accept(ModItems.RAW_ZIRCON);
+    }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
---
+++ src/main/java/net/gigavoltage/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.gigavoltage.tutorialmod.item;
+
+import net.gigavoltage.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.awt.event.InputEvent;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().stacksTo(69)));
+
+    public static RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
+            () -> new Item(new Item.Properties().stacksTo(69)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
